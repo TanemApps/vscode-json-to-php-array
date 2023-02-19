a598e7d2bf5058cc84cfb340d842b8f1f5e94d79
Émile Perron <cunrakes@gmail.com>
2023-02-18 22:01:26 -0500

    fix: improve conversion to avoid broken string values

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { convertJsonToPhpArray } from "./convert.ts";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
@@ -10,10 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
           JSON.parse(jsonValue);
 
           // Convert to PHP Array.
-          const phpArrayValue = jsonValue
-            .replaceAll("{", "[")
-            .replaceAll("}", "]")
-            .replaceAll(":", " =>");
+          const phpArrayValue = convertJsonToPhpArray(jsonValue);
 
           // Paste.
           vscode.window.activeTextEditor?.edit((editBuilder) => {
